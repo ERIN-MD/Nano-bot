3f848cc86a6e69771e26630c941cb1a78d2a6f6e
Erin-MD <144317574+Boudy1490@users.noreply.github.com>
2024-09-14 21:38:44 +0300

    Add files via upload

---
+++ plugins/messi.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*معزه اللي كريستيانو عمه 01012900090*`, m)}
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['مسي', 'ميسي']
+handler.tags = ['internet']
+handler.command = /^(ميسي|مسي)$/i
+export default handler
